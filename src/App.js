3f4055ce32abfcfc9910e55338cdbb5d286c142d
Scott Cain <scott+git@scottcain.net>
2023-03-22 20:17:07 -0700

    parameterized for GET params

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React, { useState, useEffect } from "react";
 import { RemoteFile } from "generic-filehandle";
 import NCList from "@gmod/nclist";
 
+const queryParameters = new URLSearchParams(window.location.search)
+  const assembly = queryParameters.get("assembly")
+  const release  = queryParameters.get("release")
+  const refseq   = queryParameters.get("refseq")
+  const start    = queryParameters.get("start")
+  const end      = queryParameters.get("end")
+  
+
 async function accessStore() {
   const store = new NCList({
     baseUrl:
-      "https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase/WS286/c_elegans_PRJNA13758/",
+      "https://s3.amazonaws.com/agrjbrowse/MOD-jbrowses/WormBase/WS"+release+"/"+assembly+"/",
     urlTemplate: "tracks/Curated_Genes/{refseq}/trackData.jsonz",
     readFile: (url) => new RemoteFile(url).readFile(),
   });
 
   const features = [];
   for await (const feature of store.getFeatures({
-    refName: "I",
-    start: 5720822,
-    end: 5732439,
+    refName: refseq,
+    start: start,
+    end: end,
   })) {
     console.log(
       `got feature at ${feature.get("seq_id")}:${feature.get(
